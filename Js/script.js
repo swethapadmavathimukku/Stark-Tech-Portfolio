e56ddf85eb511e982d285feafb8f450f405748e1
Sayyad Aslam <sayyadaslam2020@gmail.com>
2024-09-25 12:46:53 +0530

    Create js folder

---
+++ Js/script.js
@@ -0,0 +1,16 @@
+burger = document.querySelector('.burger');
+navbar = document.querySelector('.navbar');
+navList = document.querySelector('.nav-list');
+rightNav = document.querySelector('.rightNav');
+
+burger.addEventListener('click', ()=>{ 
+    rightNav.classList.toggle('visibility');
+    navList.classList.toggle('visibility');
+    navbar.classList.toggle('h-nav');
+
+})
+
+const currentYear = new Date().getFullYear();
+document.getElementById('year').textContent = currentYear;
+
+
